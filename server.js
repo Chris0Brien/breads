a92d6c35584c7d92b3a39ed5133b1af6b65debad
Chris O'Brien <you@example.comchristopherwobrien03@gmail.com>
2022-03-09 20:08:45 -0600

    sending index and show data

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
-})
-
 // LISTEN
 app.listen(PORT, () => {
   console.log('nomming at port', PORT);
@@ -24,3 +19,8 @@ app.get('/', (req, res) => {
   // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
